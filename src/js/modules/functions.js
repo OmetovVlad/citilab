022f960e6c7f0be0443d74147a6ce4890bc2b2f9
Vladislav Ometov <vladislavometov@MacBook-Air-Vladislav.local>
2024-06-29 04:59:35 +0700

    Update

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,6 +1,4 @@
 /* Проверка поддержки webp, добавление класса webp или no-webp для HTML */
-import events from "events";
-
 export function isWebp() {
 	// Проверка поддержки webp
 	function testWebP(callback) {
@@ -15,4 +13,58 @@ export function isWebp() {
 		let className = support === true ? 'webp' : 'no-webp';
 		document.documentElement.classList.add(className);
 	});
+}
+
+export function bsoAnalysis () {
+	const AnalysisEls = document.querySelectorAll('.bso-analys');
+
+	AnalysisEls.forEach(element => element.addEventListener('click', switchAnalysis));
+
+	function switchAnalysis(e) {
+
+		if (this.classList.contains('bso-analys--active')) {
+			this.classList.remove('bso-analys--active');
+			document.querySelector('.bso-analys-info').remove()
+		} else {
+			AnalysisEls.forEach(element => element.classList.remove('bso-analys--active'));
+			this.classList.add('bso-analys--active');
+
+			if (document.querySelector('.bso-analys-info')) {
+				document.querySelector('.bso-analys-info').remove()
+			}
+
+			let indexElement = 1;
+
+			AnalysisEls.forEach((element, index = 0) => {
+				if (element.classList.contains('bso-analys--active')) { indexElement = index + 1; }
+				index = index + 1
+			});
+
+			let itemsInLine = 1;
+
+			if (window.innerWidth >= 998) {
+				itemsInLine = 3;
+			} else if (window.innerWidth >= 768) {
+				itemsInLine = 2;
+			} else {
+				itemsInLine = 1;
+			}
+
+			let currentLine = 0;
+
+			if (indexElement % itemsInLine != 0) {
+				currentLine = Math.floor(indexElement / itemsInLine) + 1;
+			} else {
+				currentLine = indexElement / itemsInLine;
+			}
+
+			let endLineElId = currentLine * itemsInLine;
+				
+			if (endLineElId > AnalysisEls.length) {
+				endLineElId = AnalysisEls.length;
+			}
+
+			document.querySelectorAll('.bso-analys')[endLineElId - 1].insertAdjacentHTML("afterEnd", "<div class='bso-analys-info'>" + this.querySelector('.bso-analys-hidden-info').innerHTML + "</div>");
+		}
+	}
 }
